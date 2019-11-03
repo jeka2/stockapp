07815c0279c32d2f9241948b05adcdc71162596d
Yevgeniy Ivanov <anigeka@gmail.com>
2019-11-03 14:15:18 -0500

    add Web API backend

--- front-end/stock-front/src/pages/Home.js
+++ front-end/stock-front/src/pages/Home.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Login from '../components/Login';
 
 export default class Home extends Component {
     render() {
         return (
+            //transaction history
+            //ability to look up and purchase
+            //
             <>
                 <div className="personal-info">
-
+                    <Login />
                 </div>
             </>
         )
